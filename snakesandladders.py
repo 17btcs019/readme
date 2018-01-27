d46667599c1b2e3b37b4ee6eb73138b4c54c740d
17btcs019 <35477430+17btcs019@users.noreply.github.com>
2018-01-27 10:35:56 +0530

    Add files via upload

---
+++ snakesandladders.py
@@ -0,0 +1,209 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: /home/cs2017a118/amulya/snakesandladders.py ============
+2
+>>> 
+============ RESTART: /home/cs2017a118/amulya/snakesandladders.py ============
+6
+>>> 
+============ RESTART: /home/cs2017a118/amulya/snakesandladders.py ============
+3
+>>> 
+============ RESTART: /home/cs2017a118/amulya/snakesandladders.py ============
+snakes and ladders game
+Traceback (most recent call last):
+  File "/home/cs2017a118/amulya/snakesandladders.py", line 11, in <module>
+    if ladders (n):
+NameError: name 'n' is not defined
+>>> 
+============ RESTART: /home/cs2017a118/amulya/snakesandladders.py ============
+snakes and ladders game
+Traceback (most recent call last):
+  File "/home/cs2017a118/amulya/snakesandladders.py", line 11, in <module>
+    if ladders.key(n):
+AttributeError: 'dict' object has no attribute 'key'
+>>> 
+============ RESTART: /home/cs2017a118/amulya/snakesandladders.py ============
+snakes and ladders game
+Its a ladder,Climb up
+its a snake!Come down
+3
+>>> 
+============ RESTART: /home/cs2017a118/amulya/snakesandladders.py ============
+snakes and ladders game
+enter q to roll a dice
+============ RESTART: /home/cs2017a118/amulya/snakesandladders.py ============
+enter q to roll a diceq
+enter q to roll a dice
+============ RESTART: /home/cs2017a118/amulya/snakesandladders.py ============
+enter q to roll a diceq
+enter q to roll a dice1
+enter q to roll a dice
+enter q to roll a dice
+enter q to roll a dice
+enter q to roll a dice
+enter q to roll a dice
+============ RESTART: /home/cs2017a118/amulya/snakesandladders.py ============
+enter q to roll a diceq
+enter q to roll a dice
+============ RESTART: /home/cs2017a118/amulya/snakesandladders.py ============
+enter q to roll a diceq
+enter q to roll a diceq
+enter q to roll a dice
+============ RESTART: /home/cs2017a118/amulya/snakesandladders.py ============
+enter q to roll a diceq
+you got 4
+your current location is 4
+enter q to roll a diceq
+you got 3
+your current location is 7
+enter q to roll a diceq
+you got 1
+your current location is 8
+its a ladder! climb up
+enter q to roll a diceq
+you got 6
+your current location is 43
+enter q to roll a diceq
+you got 1
+your current location is 44
+enter q to roll a diceq
+you got 3
+your current location is 47
+enter q to roll a diceq
+you got 5
+your current location is 52
+its a snake! come down
+enter q to roll a diceq
+you got 4
+your current location is 12
+enter q to roll a diceq
+you got 4
+your current location is 16
+enter q to roll a diceq
+you got 2
+your current location is 18
+enter q to roll a diceq
+you got 6
+your current location is 24
+enter q to roll a diceq
+you got 5
+your current location is 29
+enter q to roll a diceq
+you got 1
+your current location is 30
+enter q to roll a diceq
+you got 3
+your current location is 33
+enter q to roll a diceq
+you got 2
+your current location is 35
+enter q to roll a diceq
+you got 3
+your current location is 38
+its a snake! come down
+enter q to roll a diceq
+you got 5
+your current location is 14
+enter q to roll a diceq
+you got 6
+your current location is 20
+enter q to roll a diceq
+you got 2
+your current location is 22
+enter q to roll a diceq
+you got 3
+your current location is 25
+its a snake! come down
+enter q to roll a diceq
+you got 3
+your current location is 7
+enter q to roll a diceq
+you got 5
+your current location is 12
+enter q to roll a diceq
+you got 5
+your current location is 17
+enter q to roll a diceq
+you got 2
+your current location is 19
+enter q to roll a diceq
+you got 3
+your current location is 22
+enter q to roll a diceq
+you got 1
+your current location is 23
+enter q to roll a diceq
+you got 3
+your current location is 26
+enter q to roll a diceq
+you got 3
+your current location is 29
+enter q to roll a diceq
+you got 4
+your current location is 33
+enter q to roll a diceq
+you got 1
+your current location is 34
+enter q to roll a diceq
+you got 1
+your current location is 35
+enter q to roll a diceq
+you got 2
+your current location is 37
+enter q to roll a diceq
+you got 2
+your current location is 39
+enter q to roll a diceq
+you got 2
+your current location is 41
+enter q to roll a diceq
+you got 2
+your current location is 43
+enter q to roll a diceq
+you got 2
+your current location is 45
+enter q to roll a diceq
+you got 1
+your current location is 46
+enter q to roll a diceq
+you got 4
+your current location is 50
+enter q to roll a diceq
+you got 5
+your current location is 55
+enter q to roll a diceq
+you got 2
+your current location is 57
+enter q to roll a diceq
+you got 4
+your current location is 61
+enter q to roll a diceq
+you got 2
+your current location is 63
+enter q to roll a diceq
+you got 3
+your current location is 66
+enter q to roll a diceq
+you got 5
+your current location is 71
+enter q to roll a diceq
+you got 3
+your current location is 74
+enter q to roll a diceq
+you got 1
+your current location is 75
+enter q to roll a diceq
+you got 1
+your current location is 76
+its a ladder! climb up
+enter q to roll a diceq
+you won
+>>> q
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    q
+NameError: name 'q' is not defined
+>>> 
