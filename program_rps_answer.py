62c649d6dfb422c34735fc34edba026a46082187
17btcs019 <35477430+17btcs019@users.noreply.github.com>
2018-02-20 09:49:12 +0530

    Add files via upload

---
+++ program_rps_answer.py
@@ -0,0 +1,30 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: /home/cs2017a118/amulya/program_rockpaperscissors.py =======
+Traceback (most recent call last):
+  File "/home/cs2017a118/amulya/program_rockpaperscissors.py", line 1, in <module>
+    fhfd
+NameError: name 'fhfd' is not defined
+>>> 
+======= RESTART: /home/cs2017a118/amulya/program_rockpaperscissors.py =======
+Rock, Paper, Scissors?
+That's not a valid play. Check your spelling!
+Rock, Paper, Scissors?paper
+That's not a valid play. Check your spelling!
+Rock, Paper, Scissors?Paper
+Tie!
+Rock, Paper, Scissors?Scissors
+Tie!
+Rock, Paper, Scissors?Scissors
+You lose... Rock smashes Scissors
+Rock, Paper, Scissors?Rock
+Tie!
+Rock, Paper, Scissors?Paper
+Tie!
+Rock, Paper, Scissors?Scissors
+Tie!
+Rock, Paper, Scissors?Paper
+You win! Paper covers Rock
+Rock, Paper, Scissors?
